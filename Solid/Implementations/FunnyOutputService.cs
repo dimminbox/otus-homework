baa7c69d1aed4e82fbd53d62d58f7e5245a550f2
loginov.dn <loginovdn.it@relef.ru>
2026-03-30 19:25:42 +0300

    solid

--- Solid/Implementations/FunnyOutputService.cs
+++ Solid/Implementations/FunnyOutputService.cs
@@ -2,10 +2,10 @@ namespace Solid.Implementations;
 
 public class FunnyOutputService : BaseOutputService
 {
-    protected override string WonMessage => "🎉 Splendid! You spot on!";
-    protected override string LoseMessage => "💀 Unfortunately! Game over.";
-    protected override string MoreMessage => "⬆️ It's more than a goal.";
-    protected override string LessMessage => "⬇️ It's less than a goal.";
+    protected override string WonMessage => "Splendid! You spot on!";
+    protected override string LoseMessage => "Unfortunately! Game over.";
+    protected override string MoreMessage => " It's more than a goal.";
+    protected override string LessMessage => " It's less than a goal.";
 
     protected override ConsoleColor WinColor => ConsoleColor.Magenta;
     protected override ConsoleColor LoseColor => ConsoleColor.DarkRed;
